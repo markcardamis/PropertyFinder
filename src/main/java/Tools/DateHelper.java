7928ec5ce5da66f907906239a91095690d757ca9
markcardamis <markncardamis@gmail.com>
2018-09-06 22:09:59 +1000

    add date debug string

--- src/main/java/Tools/DateHelper.java
+++ src/main/java/Tools/DateHelper.java
@@ -6,11 +6,13 @@ import java.util.TimeZone;
 public class DateHelper {
     public boolean isBusinessDay(){
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Australia/Sydney"));
+        Integer hours = (cal.get(Calendar.HOUR_OF_DAY));
 
         // check if weekend
         if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
             return false;
-        } else if ((cal.get(Calendar.HOUR_OF_DAY) < 9) || (cal.get(Calendar.HOUR_OF_DAY) > 18)){
+        } else if (( hours < 9) || (hours > 18)){
+            System.out.print(hours.toString());
             return false;
         }
         // IF NOTHING ELSE, IT'S A BUSINESS DAY
