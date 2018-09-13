3e012c0231ca65500de3aa99a87d9e96d2648986
markcardamis <markncardamis@gmail.com>
2018-09-13 17:13:17 +1000

    adding comment to test heroku auto deploy 20

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -50,6 +50,7 @@ public class MainTest {
 
     private void getDomainAuth() throws Exception {
         String authKey = System.getenv().get("authKey");
+        System.out.println("AuthKey is " + authKey);
         DomainAuthentication domainAuthentication = new DomainAuthentication();
         DomainTokenAuthResponse domainTokenAuthResponse = domainAuthentication.getAuthToken(authKey);
         authToken = domainTokenAuthResponse.access_token;
