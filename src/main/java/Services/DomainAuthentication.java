361087bda85a287996f765d9f2622288590c4129
markcardamis <markncardamis@gmail.com>
2018-09-16 18:48:04 +1000

    adding comment to test heroku auto deploy 61

--- src/main/java/Services/DomainAuthentication.java
+++ src/main/java/Services/DomainAuthentication.java
@@ -18,7 +18,7 @@ public class DomainAuthentication implements IDomainAuthentication
     public DomainTokenAuthResponse getAuthToken(String authKey) throws Exception{
         String urlAuth = "https://auth.domain.com.au/v1/connect/token/";
         String responseJson = mServiceHelper.callHTTPService(urlAuth, HttpMethod.POST, 
-        "grant_type=client_credentials&scope=api_agencies_read api_listings_read", true, authKey);
+        "grant_type=client_credentials&scope=api_listings_read", true, authKey);
         Gson gson = new Gson();
         return gson.fromJson(responseJson, DomainTokenAuthResponse.class);
     }
