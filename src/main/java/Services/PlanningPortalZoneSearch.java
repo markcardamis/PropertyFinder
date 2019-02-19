582e4f851dc0a7aaae2f89572698a47cc266cd36
markcardamis <markncardamis@gmail.com>
2019-02-19 19:46:11 +1100

    added check for lot numbers in Address

--- src/main/java/Services/PlanningPortalZoneSearch.java
+++ src/main/java/Services/PlanningPortalZoneSearch.java
@@ -27,20 +27,24 @@ public class PlanningPortalZoneSearch implements IPlanningPortalZoneSearch
                     propertyListings[i].lgaName = "0";
                     propertyListings[i].lgaCode = 0;
 
-                    String address = urlPlanningPortal + propertyListings[i].planningPortalURL + "/land_zoning";
-                    String responseJson = mServiceHelper.callHTTPService(address,
-                            HttpMethod.GET, "", false, "");
-                    Gson gson = new Gson();
-                    PlanningPortalZoneResponse planningPortalZoneResponse = gson.fromJson(responseJson, PlanningPortalZoneResponse.class);
-                    propertyListings[i].zone = planningPortalZoneResponse.data.results[0].display_content;
-                    for (int j = 0; j < planningPortalZoneResponse.data.results[0].attributes.size(); j++) {
-                        if (planningPortalZoneResponse.data.results[0].attributes.get(j).id == 10) {
-                            propertyListings[i].lgaName =
-                                    planningPortalZoneResponse.data.results[0].attributes.get(j).field_value;
-                        } else if (planningPortalZoneResponse.data.results[0].attributes.get(j).id == 11 &&
-                                planningPortalZoneResponse.data.results[0].attributes.get(j).field_value.matches("\\d+")) {
-                            propertyListings[i].lgaCode =
-                                    Integer.valueOf(planningPortalZoneResponse.data.results[0].attributes.get(j).field_value);
+                    if (propertyListings[i].address.toLowerCase().contains("lot")) { // lot numbers don't always get correct zone
+                        propertyListings[i].zone = "TBC";
+                    } else {
+                        String address = urlPlanningPortal + propertyListings[i].planningPortalURL + "/land_zoning";
+                        String responseJson = mServiceHelper.callHTTPService(address,
+                                HttpMethod.GET, "", false, "");
+                        Gson gson = new Gson();
+                        PlanningPortalZoneResponse planningPortalZoneResponse = gson.fromJson(responseJson, PlanningPortalZoneResponse.class);
+                        propertyListings[i].zone = planningPortalZoneResponse.data.results[0].display_content;
+                        for (int j = 0; j < planningPortalZoneResponse.data.results[0].attributes.size(); j++) {
+                            if (planningPortalZoneResponse.data.results[0].attributes.get(j).id == 10) {
+                                propertyListings[i].lgaName =
+                                        planningPortalZoneResponse.data.results[0].attributes.get(j).field_value;
+                            } else if (planningPortalZoneResponse.data.results[0].attributes.get(j).id == 11 &&
+                                    planningPortalZoneResponse.data.results[0].attributes.get(j).field_value.matches("\\d+")) {
+                                propertyListings[i].lgaCode =
+                                        Integer.valueOf(planningPortalZoneResponse.data.results[0].attributes.get(j).field_value);
+                            }
                         }
                     }
                 } catch (Exception e) {
