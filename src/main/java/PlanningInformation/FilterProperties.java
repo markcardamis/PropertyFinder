4023ff01543ded859c89deec8d2616ca068d9478
markcardamis <markncardamis@gmail.com>
2019-02-19 19:49:52 +1100

    changed filtering

--- src/main/java/PlanningInformation/FilterProperties.java
+++ src/main/java/PlanningInformation/FilterProperties.java
@@ -21,41 +21,21 @@ public class FilterProperties implements IFilterProperties {
                     priceInt = PriceMethods.stringToInteger(propertyListings[i].price);
                     pricePerArea = priceInt/Math.round(propertyListings[i].area);
 
-                    if (propertyListings[i].lgaCode == 900) { //BLUE MOUNTAINS
-                        if ((propertyListings[i].zone.equals("R1")) &&
-                                (propertyListings[i].area > 1350) && (pricePerArea < 400)) {
-                            System.out.println(propertyListings[i].listingURL);
-                        } else if ((propertyListings[i].zone.equals("DM")) &&
-                                (propertyListings[i].area > 1200) && (pricePerArea < 300)) {
-                            System.out.println(propertyListings[i].listingURL);
-                        } else if ((propertyListings[i].zone.equals("R3")) &&
-                                (propertyListings[i].area > 1350) && (pricePerArea < 400)) {
-                            System.out.println(propertyListings[i].listingURL);
-                        } else if ((propertyListings[i].zone.equals("R2")) &&
-                                (propertyListings[i].area > 1200) && (priceInt < 300000)) {
-                            System.out.println(propertyListings[i].listingURL);
-                        } else if (keywordExists.isKeywordPresent(propertyListings[i].summaryDescription, keywords)) {
-                            System.out.println("Property keyword " + propertyListings[i].listingURL);
-                        } else {
-                            propertyListings = ArrayUtils.remove(propertyListings, i);
-                            i--;
-                        }
-                    } else { //generic response
-                        if ((propertyListings[i].zone.equals("R1")) &&
-                                (propertyListings[i].area > 1350) && (pricePerArea < 400)) {
-                            System.out.println("R1 " + propertyListings[i].listingURL);
-                        } else if ((propertyListings[i].zone.equals("R3")) &&
-                                (propertyListings[i].area > 1350) && (pricePerArea < 400)) {
-                            System.out.println("R3 " + propertyListings[i].listingURL);
-                        } else if ((propertyListings[i].zone.equals("R4")) &&
-                                (propertyListings[i].area > 1350)) {
-                            System.out.println("R4 " + propertyListings[i].listingURL);
-                        }
-                        else {
-                            propertyListings = ArrayUtils.remove(propertyListings, i);
-                            i--;
-                        }
+                    if ((propertyListings[i].zone.equals("R1")) &&
+                            (propertyListings[i].area > 1350) && (pricePerArea < 400)) {
+                        System.out.println("R1 " + propertyListings[i].listingURL);
+                    } else if ((propertyListings[i].zone.equals("R3")) &&
+                            (propertyListings[i].area > 1350) && (pricePerArea < 400)) {
+                        System.out.println("R3 " + propertyListings[i].listingURL);
+                    } else if ((propertyListings[i].zone.equals("R4")) &&
+                            (propertyListings[i].area > 1350)) {
+                        System.out.println("R4 " + propertyListings[i].listingURL);
                     }
+                    else {
+                        propertyListings = ArrayUtils.remove(propertyListings, i);
+                        i--;
+                    }
+                    
 
                 }
             } catch (Exception e) {
