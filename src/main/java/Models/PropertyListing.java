8fb5bbefe170c0cc4c50c537c4dab36955b2132f
markcardamis <markncardamis@gmail.com>
2019-06-13 21:38:25 +1000

    added minimum Lot Size to model

--- src/main/java/Models/PropertyListing.java
+++ src/main/java/Models/PropertyListing.java
@@ -12,4 +12,5 @@ public class PropertyListing {
     public String planningPortalPropId;
     public String planningPortalAddress;
     public String summaryDescription;
+    public String minimumLotSize;
 }
