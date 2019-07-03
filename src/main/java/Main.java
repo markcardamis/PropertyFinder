2a79108ffb4c7551c97bb91b6ceaadef73a81f4a
markcardamis <markncardamis@gmail.com>
2019-07-03 18:46:30 +1000

    rolled back delay in schedule

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
       try {
         Timer t = new Timer();
         MyTask mTask = new MyTask();
-        t.scheduleAtFixedRate(mTask, 1000L, 3600000L); // Run every hour
+        t.scheduleAtFixedRate(mTask, 0L, 3600000L); // Run every hour
       } catch (Exception e) {
         System.out.println("Scheduling Exception " + e.getMessage());
       }
