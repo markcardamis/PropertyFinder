4ae549e76d00ec15f670210ca09139651404e3c8
markcardamis <markncardamis@gmail.com>
2019-07-11 17:59:28 +1000

    chjanged filter properties

--- src/main/java/PlanningInformation/FilterProperties.java
+++ src/main/java/PlanningInformation/FilterProperties.java
@@ -23,36 +23,42 @@ public class FilterProperties implements IFilterProperties {
                     priceInt = PriceMethods.stringToInteger(propertyListings[i].price);
                     pricePerArea = priceInt/Math.round(propertyListings[i].area);
 
-                    if ((propertyListings[i].zone.equals("R1")) &&
-                            (propertyListings[i].area > 1350)) {
+                    if ((propertyListings[i].zone.equals("R1")) && (propertyListings[i].area > 1350)) {
                         propertyListings[i].selectionReason = "R1, >1350m";
                         System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
-                    } else if ((propertyListings[i].zone.equals("R3")) &&
-                            (propertyListings[i].area > 1350)) {
+                    } else if (propertyListings[i].zone.equals("R1")) {
+                        propertyListings[i].selectionReason = "R1";
+                        System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
+                    } else if ((propertyListings[i].zone.equals("R3")) && (propertyListings[i].area > 1350)) {
                         propertyListings[i].selectionReason = "R3, >1350m";
                         System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
-                    } else if ((propertyListings[i].zone.equals("R4")) &&
-                            (propertyListings[i].area > 400)) {
+                    } else if (propertyListings[i].zone.equals("R3")) {
+                        propertyListings[i].selectionReason = "R3";
+                        System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
+                    } else if ((propertyListings[i].zone.equals("R4")) && (propertyListings[i].area > 400)) {
                         propertyListings[i].selectionReason = "R4, >400m";
                         System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
-                    } else if ((propertyListings[i].zone.contains("B")) &&
-                            (propertyListings[i].area > 400)) {
+                    } else if (propertyListings[i].zone.equals("R4")){
+                        propertyListings[i].selectionReason = "R4";
+                        System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
+                    } else if ((propertyListings[i].zone.contains("B")) && (propertyListings[i].area > 400)) {
                         propertyListings[i].selectionReason = "B, >400m";
                         System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
-                    } else if ((propertyListings[i].zone.contains("E4")) &&
-                            (propertyListings[i].area > 4000) && (pricePerArea < 100) &&
-                            (keywordExists.isKeywordPresent(propertyListings[i].postCode, postcodes))){
+                    } else if (propertyListings[i].zone.contains("B")) {
+                        propertyListings[i].selectionReason = "B";
+                        System.out.println(propertyListings[i].zone + " " + propertyListings[i].listingURL);
+                    } else if ((propertyListings[i].zone.contains("E4")) && (propertyListings[i].area > 4000) &&
+                            (pricePerArea < 100) && (keywordExists.isKeywordPresent(propertyListings[i].postCode, postcodes))){
                         propertyListings[i].selectionReason = "E4, Postcode, >4000m, $100m<";
                         System.out.println(propertyListings[i].zone + " PostCode " + propertyListings[i].listingURL);
                     } else if ((keywordExists.isKeywordPresent(propertyListings[i].summaryDescription, keywords)) &&
                     (keywordExists.isKeywordPresent(propertyListings[i].postCode, postcodes))){
-                        propertyListings[i].selectionReason = "Keyword found: " +
-                                keywordExists.keywordPresent(propertyListings[i].summaryDescription, keywords);
+                        propertyListings[i].selectionReason = "Keyword found: " + propertyListings[i].summaryDescription;
                         System.out.println("Keyword found: " + propertyListings[i].listingURL);
                     } else {
                         propertyListings = ArrayUtils.remove(propertyListings, i);
                         i--;
-                    }                
+                    }            
 
                 }
             } catch (Exception e) {
