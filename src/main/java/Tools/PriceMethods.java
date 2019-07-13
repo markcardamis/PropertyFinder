8b3dc6e9bc525d2fc24a3dc5373046925e114016
markcardamis <markncardamis@gmail.com>
2019-07-14 01:11:50 +1000

    changed price methods class

--- src/main/java/Tools/PriceMethods.java
+++ src/main/java/Tools/PriceMethods.java
@@ -4,26 +4,40 @@ package Tools;
 
 public class PriceMethods
 {
-    public static Integer stringToInteger(String text) throws Exception{
+    public static Integer stringToInteger(String text) throws Exception {
 
-        int firstIndex = text.indexOf('$');
-        if (firstIndex >= 0) { // found a $
-            text = text.substring(firstIndex+1);
+        String errorNumber = "10000000";
 
-            if (text.length() < 12) {
-                text = text.replaceAll("[^0-9.]", "");
-            } else {
+        if(text.matches(".*\\d.*")) {
+            int firstIndex = text.indexOf('$');
+            if (firstIndex >= 0) { // found a $
+                text = text.substring(firstIndex + 1);
+
                 int secondIndex = text.indexOf('$');
-                if (secondIndex > 0){ // found a second $
+                if (secondIndex > 0) {
                     text = text.substring(0, secondIndex);
-                    text = text.replaceAll("[^0-9.]", "");
-                } else {
-                    text = "10000000";
                 }
+                if (text.length() < 6) {
+                    if (text.contains("m") || text.contains("M")) {
+                        text = text.replaceAll("[^0-9.]", "");
+                        return ((int) (Double.parseDouble(text) * 1000000));
+                    } else if (text.contains("k") || text.contains("K")) {
+                        text = text.replaceAll("[^0-9.]", "");
+                        return ((int) (Double.parseDouble(text) * 1000));
+                    } else {
+                        text = text.replaceAll("[^0-9.]", "");
+                        return ((int) (Double.parseDouble(text) * 1000000));
+                    }
+                } else {
+                    text = text.replaceAll("[^0-9.]", "");
+                }
+            } else {
+                text = errorNumber;
             }
         } else {
-            text = "10000000";
+            text = errorNumber;
         }
+
         return ((int) Double.parseDouble(text));
     }
 
