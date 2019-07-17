300a9c33ed3fb45c352e4320acb8c460850b4028
markcardamis <markncardamis@gmail.com>
2019-07-17 20:46:08 +1000

    added more filters to pricemethods parser

--- src/main/java/Tools/PriceMethods.java
+++ src/main/java/Tools/PriceMethods.java
@@ -10,22 +10,40 @@ public class PriceMethods
 
         if(text.matches(".*\\d.*")) {
             int firstIndex = text.indexOf('$');
+            text = text.toLowerCase();
             if (firstIndex >= 0) { // found a $
                 text = text.substring(firstIndex + 1);
 
-                int secondIndex = text.indexOf('$');
+                int secondIndex = text.indexOf('-');
+                int thirdIndex = text.indexOf("to");
+                int fourthIndex = text.indexOf('$');
                 if (secondIndex > 0) {
                     text = text.substring(0, secondIndex);
+                } else if (thirdIndex > 0) {
+                    text = text.substring(0, thirdIndex);
+                } else if (fourthIndex > 0) {
+                    text = text.substring(0, fourthIndex);
                 }
+                if (text.contains("mil") || text.contains("million")) {
+                    text = text.replaceAll("[^0-9.]", "");
+                    text = removeMultipleDecimals(text);
+                    return ((int) (Double.parseDouble(text) * 1000000));
+                }
+
+                text = text.replaceAll("[,|\\s]", "");
+
                 if (text.length() < 6) {
-                    if (text.contains("m") || text.contains("M")) {
+                    if (text.contains("m")) {
                         text = text.replaceAll("[^0-9.]", "");
+                        text = removeMultipleDecimals(text);
                         return ((int) (Double.parseDouble(text) * 1000000));
-                    } else if (text.contains("k") || text.contains("K")) {
+                    } else if (text.contains("k")) {
                         text = text.replaceAll("[^0-9.]", "");
+                        text = removeMultipleDecimals(text);
                         return ((int) (Double.parseDouble(text) * 1000));
                     } else {
                         text = text.replaceAll("[^0-9.]", "");
+                        text = removeMultipleDecimals(text);
                         return ((int) (Double.parseDouble(text) * 1000000));
                     }
                 } else {
@@ -38,7 +56,19 @@ public class PriceMethods
             text = errorNumber;
         }
 
+        text = removeMultipleDecimals(text);
         return ((int) Double.parseDouble(text));
+    }
+
+    private static String removeMultipleDecimals(String text) {
+        if (text.matches(".*\\..*\\..*")) {
+            int firstIndex = text.indexOf('.');
+            int secondIndex = text.indexOf('.', firstIndex + 1);
+            if (secondIndex > 0) {
+                text = text.substring(0, secondIndex);
+            }
+        }
+        return text;
     }
 
 }
