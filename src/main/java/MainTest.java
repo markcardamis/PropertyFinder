25a0e6d466e044dee25d44931ab9391326a1f35d
markcardamis <markncardamis@gmail.com>
2019-07-26 00:52:39 +1000

    .

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -17,7 +17,7 @@ public class MainTest {
     PropertyListing[] propertyListings;
     PropertyListing[] propertyListingsComplete = null;
     PropertySearchRequest searchJson;
-    Integer domainKey = 1;
+    Integer domainKey = 2;
     Integer domainSearchCount = 0;
     String[] authKey = {
         System.getenv().get("DOMAIN_KEY_0"),
