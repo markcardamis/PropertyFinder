85b5fbbd3e85f01055f3af7de2fcbcc1724bca14
markcardamis <markncardamis@gmail.com>
2019-08-23 01:37:48 +1000

    test price

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -51,7 +51,7 @@ public class MainTest {
         Integer priceIncrementAmountSmallRegional = 20000;
         Integer priceIncrementAmountMedium = 200000;
         Integer priceIncrementAmountLarge = 1000000;
-        Integer priceStop = 5000000;
+        Integer priceStop = 120000;
         Integer minLandSize = 400;
         String[] propertyTypes = new String[]{"DevelopmentSite", "House", "NewLand", "VacantLand"};
 
