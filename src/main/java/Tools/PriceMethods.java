c2471af335311db5fe885da8cc98268ca77f969a
markcardamis <markncardamis@gmail.com>
2019-08-23 01:33:22 +1000

    added more checks to pricemethods

--- src/main/java/Tools/PriceMethods.java
+++ src/main/java/Tools/PriceMethods.java
@@ -8,55 +8,59 @@ public class PriceMethods
 
         String errorNumber = "10000000";
 
-        if(text != null && text.matches(".*\\d.*")) {
-            text = text.toLowerCase();
-            int firstIndex = text.indexOf('$');
-            if (firstIndex >= 0) { // found a $
-                text = text.substring(firstIndex + 1);
+        try {
+            if (text != null && !text.isEmpty() && text.matches(".*\\d.*")) {
+                text = text.toLowerCase();
+                int firstIndex = text.indexOf('$');
+                if (firstIndex >= 0) { // found a $
+                    text = text.substring(firstIndex + 1);
 
-                int secondIndex = text.indexOf('-');
-                int thirdIndex = text.indexOf("to");
-                int fourthIndex = text.indexOf('$');
-                if (secondIndex > 0) {
-                    text = text.substring(0, secondIndex);
-                } else if (thirdIndex > 0) {
-                    text = text.substring(0, thirdIndex);
-                } else if (fourthIndex > 0) {
-                    text = text.substring(0, fourthIndex);
-                }
-                if (text.contains("mil") || text.contains("million")) {
-                    text = text.replaceAll("[^0-9.]", "");
-                    text = removeMultipleDecimals(text);
-                    return ((int) (Double.parseDouble(text) * 1000000));
-                }
-
-                text = text.replaceAll("[,|\\s]", "");
-
-                if (text.length() < 6) {
-                    if (text.contains("m")) {
-                        text = text.replaceAll("[^0-9.]", "");
-                        text = removeMultipleDecimals(text);
-                        return ((int) (Double.parseDouble(text) * 1000000));
-                    } else if (text.contains("k")) {
-                        text = text.replaceAll("[^0-9.]", "");
-                        text = removeMultipleDecimals(text);
-                        return ((int) (Double.parseDouble(text) * 1000));
-                    } else {
+                    int secondIndex = text.indexOf('-');
+                    int thirdIndex = text.indexOf("to");
+                    int fourthIndex = text.indexOf('$');
+                    if (secondIndex > 0) {
+                        text = text.substring(0, secondIndex);
+                    } else if (thirdIndex > 0) {
+                        text = text.substring(0, thirdIndex);
+                    } else if (fourthIndex > 0) {
+                        text = text.substring(0, fourthIndex);
+                    }
+                    if (text.contains("mil") || text.contains("million")) {
                         text = text.replaceAll("[^0-9.]", "");
                         text = removeMultipleDecimals(text);
                         return ((int) (Double.parseDouble(text) * 1000000));
                     }
+
+                    text = text.replaceAll("[,|\\s]", "");
+
+                    if (text.length() < 6) {
+                        if (text.contains("m")) {
+                            text = text.replaceAll("[^0-9.]", "");
+                            text = removeMultipleDecimals(text);
+                            return ((int) (Double.parseDouble(text) * 1000000));
+                        } else if (text.contains("k")) {
+                            text = text.replaceAll("[^0-9.]", "");
+                            text = removeMultipleDecimals(text);
+                            return ((int) (Double.parseDouble(text) * 1000));
+                        } else {
+                            text = text.replaceAll("[^0-9.]", "");
+                            text = removeMultipleDecimals(text);
+                            return ((int) (Double.parseDouble(text) * 1000000));
+                        }
+                    } else {
+                        text = text.replaceAll("[^0-9.]", "");
+                    }
                 } else {
-                    text = text.replaceAll("[^0-9]", "");
+                    text = errorNumber;
                 }
             } else {
                 text = errorNumber;
             }
-        } else {
-            text = errorNumber;
-        }
 
-        text = removeMultipleDecimals(text);
+            text = removeMultipleDecimals(text);
+        } catch (Exception e){
+            return (int) Double.parseDouble(errorNumber);
+        }
         return ((int) Double.parseDouble(text));
     }
 
