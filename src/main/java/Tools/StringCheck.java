81d809445ee33c09f91f156297d95d1fc12021e1
markcardamis <markncardamis@gmail.com>
2019-09-08 19:10:34 +1000

    adde concatWhenNotNull method

--- src/main/java/Tools/StringCheck.java
+++ src/main/java/Tools/StringCheck.java
@@ -11,4 +11,10 @@ public class StringCheck {
     public static boolean isNotNullOrEmpty(String str) {
         return(str != null && !str.isEmpty());
     }
+
+    public static String concatWhenNotNull (String originalString, String stringAdded, String delimiter) {
+        if (isNotNullOrEmpty(delimiter))
+            stringAdded += delimiter;
+            return (isNotNullOrEmpty(originalString) ? originalString + stringAdded: stringAdded);
+    }
 }
