0801361b72f398db45569eeef3c598c81da8eb8a
markcardamis <markncardamis@gmail.com>
2019-09-08 22:33:26 +1000

    added landValue method in MainTest

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -41,6 +41,7 @@ public class MainTest {
         getListingsCommercialNSW();
         propertyListingsComplete = addPlanningPortalAddress(propertyListingsComplete);
         propertyListingsComplete = addPlanningPortalZone(propertyListingsComplete);
+        propertyListingsComplete = addLandValue(propertyListingsComplete);
         propertyListingsComplete = filterProperties(propertyListingsComplete);
         saveDatabasePoint(propertyListingsComplete);
         sendEmailNotifications(propertyListingsComplete);    
@@ -54,7 +55,7 @@ public class MainTest {
         Integer priceIncrementAmountSmallRegional = 20000;
         Integer priceIncrementAmountMedium = 200000;
         Integer priceIncrementAmountLarge = 1000000;
-        Integer priceStop = 150000;
+        Integer priceStop = 1500000;
         Integer minLandSize = 400;
         String[] propertyTypes = new String[]{"DevelopmentSite", "House", "NewLand", "VacantLand"};
 
