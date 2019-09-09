2620da3bcb6f8e2a293aba624c41fef39e95b3b0
markcardamis <markncardamis@gmail.com>
2019-09-09 18:32:28 +1000

    added sydneyproperties.csv, update calling method

--- src/main/java/Services/LandValueSearch.java
+++ src/main/java/Services/LandValueSearch.java
@@ -11,11 +11,11 @@ public class LandValueSearch implements ILandValueSearch
         // Get Land Value info
         if (propertyListings.length > 0) {
             String root = System.getProperty("user.dir");
-            String csvFile = root + "/public/landvalues/bluemountains.csv";
+            String csvFile = root + "/public/landvalues/sydneyproperties.csv";
             CSVUtils csvUtils = new CSVUtils();
             for (int i = 0; i < propertyListings.length; i++){
                 try {
-                    String result = csvUtils.getPropertyField(csvFile, 2, propertyListings[i].planningPortalPropId, 15);
+                    String result = csvUtils.getPropertyField(csvFile, 0, propertyListings[i].planningPortalPropId, 1);
                     if (result != null && !result.isEmpty()) {
                         propertyListings[i].landValue = Integer.valueOf(result);
                         System.out.println("landValue " + propertyListings[i].landValue + " " + String.valueOf(i+1) + "/" + String.valueOf(propertyListings.length));
