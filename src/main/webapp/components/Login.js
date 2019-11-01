b5c1bb66274c077b62ac4933bae0c016bbe0e58d
markcardamis <markncardamis@gmail.com>
2019-11-01 22:13:49 +1100

    removed console.log

--- src/main/webapp/components/Login.js
+++ src/main/webapp/components/Login.js
@@ -26,8 +26,7 @@ export default withAuth(class Login extends Component {
   render() {
     if (this.state.authenticated === null) return null;
     return this.state.authenticated ?
-      // <Redirect to={{ pathname: '/' }}/> :
-      console.log('authenticted') :
+       <Redirect to={{ pathname: '/' }}/> :
       <LoginForm baseUrl={this.props.baseUrl} />;
   }
 });
