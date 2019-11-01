317d04e33c754b0256fa958f62f37c9d028f368b
markcardamis <markncardamis@gmail.com>
2019-11-02 02:13:51 +1100

    added okta account user info route backend

--- src/main/java/com/majoapps/propertyfinder/web/service/AccountServiceController.java
+++ src/main/java/com/majoapps/propertyfinder/web/service/AccountServiceController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +19,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5000")
 @RequestMapping(value="/api/account")
 public class AccountServiceController {
 
@@ -35,6 +41,12 @@ public class AccountServiceController {
     public List<Account> getAccountById(@PathVariable(value="id") UUID id) {
         return this.accountService.getAccount(id);
     }
+
+    @RequestMapping(value="/profile", method= RequestMethod.GET)
+        @PreAuthorize("hasAuthority('SCOPE_profile')")
+        public Map<String, Object> getUserDetails(JwtAuthenticationToken authentication) {
+            return authentication.getTokenAttributes();
+        }
 
     @RequestMapping(method= RequestMethod.POST)
     @ResponseStatus(code = HttpStatus.CREATED)
