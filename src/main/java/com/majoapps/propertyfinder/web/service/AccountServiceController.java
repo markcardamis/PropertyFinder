c28f55d9fa9b722cbbdab669b7c08a36f3285848
markcardamis <markncardamis@gmail.com>
2019-11-20 18:05:24 +1100

    .

--- src/main/java/com/majoapps/propertyfinder/web/service/AccountServiceController.java
+++ src/main/java/com/majoapps/propertyfinder/web/service/AccountServiceController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,10 +39,10 @@ public class AccountServiceController {
         return this.accountService.getAccount(id);
     }
 
-    @RequestMapping(value="{userId}", method= RequestMethod.GET)
-    public List<Account> getAccountByUserId(@PathVariable(value="id") String userId) {
-        return this.accountService.getAccountByUserId(userId);
-    }
+    // @RequestMapping(value="/{user_id}", method= RequestMethod.GET)
+    // public List<Account> getAccountByUserId(@RequestParam(value="user_id") String userId) {
+    //     return this.accountService.getAccountByUserId(userId);
+    // }
 
     @RequestMapping(method= RequestMethod.POST)
     @ResponseStatus(code = HttpStatus.CREATED)
