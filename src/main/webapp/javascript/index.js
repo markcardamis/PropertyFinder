c0f348174467b7e1503f79a09d5caeaef940f9e5
Iryna Paliy <irapaliy@gmail.com>
2019-11-28 08:24:58 +0200

    Redux

--- src/main/webapp/javascript/index.js
+++ src/main/webapp/javascript/index.js
@@ -1,8 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from '../components/App';
+// import * as reducers from '../store/reducers';
+
+// const store = createStore(combineReducers(reducers), applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
-    document.getElementById("root")
+    
+    <App /> , document.getElementById('root')
 );
+
+{/* <Provider store={store}></Provider>, */}
