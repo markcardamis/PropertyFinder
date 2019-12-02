3aac2e2d256f7f397825e879e25bfb099f7da16a
markcardamis <markncardamis@gmail.com>
2019-12-02 22:58:27 +1100

    added propertyListingService route

---
+++ src/main/java/com/majoapps/propertyfinder/web/service/PropertyListingServiceController.java
@@ -0,0 +1,30 @@
+package com.majoapps.propertyfinder.web.service;
+
+import com.majoapps.propertyfinder.business.service.PropertyListingService;
+import com.majoapps.propertyfinder.data.entity.PropertyListing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api/listing")
+public class PropertyListingServiceController {
+
+    @Autowired
+    private PropertyListingService propertyListingService;
+
+    @RequestMapping(method= RequestMethod.GET)
+    public List<PropertyListing> getAllListings() {
+        return this.propertyListingService.getAllListings();
+    }
+
+    @RequestMapping(value="{id}", method= RequestMethod.GET)
+    public PropertyListing getListingById(@PathVariable(value="id") Integer id) {
+        return this.propertyListingService.getPropertyListing(id);
+    }
+
+}
