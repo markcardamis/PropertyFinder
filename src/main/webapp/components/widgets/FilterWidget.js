440dea0868989bccfa682300c00159abef4dfc54
Iryna Paliy <irapaliy@gmail.com>
2019-12-16 00:27:02 +0200

    changed handleEditFilter

--- src/main/webapp/components/widgets/FilterWidget.js
+++ src/main/webapp/components/widgets/FilterWidget.js
@@ -33,10 +33,21 @@ class FilterWidget extends Component {
 
         async handleEditFilter (item) {
         console.log('edit filter works');
-        console.log(item);
-            this.displayFilterParameters(item);
-            this.setState({editedFilter: item})
-            // this.getFilterList();
+
+        this.setState({editedFilter: item})
+
+        this.props.dispatch(change('filter', 'propertyZone', item.propertyZone));
+        this.props.dispatch(change('filter', 'propertyAreaMin', item.propertyAreaMin));
+        this.props.dispatch(change('filter', 'propertyAreaMax', item.propertyAreaMax));
+        this.props.dispatch(change('filter', 'propertyPriceMin', item.propertyPriceMin));
+        this.props.dispatch(change('filter', 'propertyPriceMax', item.propertyPriceMax));
+        this.props.dispatch(change('filter', 'propertyPricePSMMin', item.propertyPricePSMMin));
+        this.props.dispatch(change('filter', 'propertyPricePSMMax', item.propertyPricePSMMax));
+        this.props.dispatch(change('filter', 'propertyPostCode', item.propertyPostCode));
+        this.props.dispatch(change('filter', 'propertyPriceToLandValueMin', item.propertyPriceToLandValueMin));
+        this.props.dispatch(change('filter', 'propertyPriceToLandValueMax', item.propertyPriceToLandValueMax));
+        this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMin', item.propertyFloorSpaceRatioMin));
+        this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMax', item.propertyPriceToLandValueMin));            
           }
 
                 
@@ -143,22 +154,22 @@ class FilterWidget extends Component {
     //     }
 
 
-displayFilterParameters = (item) => {
-            console.log(this.props)
-            console.log(item)
-            this.props.dispatch(change('filter', 'propertyZone', item.propertyZone));
-            this.props.dispatch(change('filter', 'propertyAreaMin', item.propertyAreaMin));
-            this.props.dispatch(change('filter', 'propertyAreaMax', item.propertyAreaMax));
-            this.props.dispatch(change('filter', 'propertyPriceMin', item.propertyPriceMin));
-            this.props.dispatch(change('filter', 'propertyPriceMax', item.propertyPriceMax));
-            this.props.dispatch(change('filter', 'propertyPricePSMMin', item.propertyPricePSMMin));
-            this.props.dispatch(change('filter', 'propertyPricePSMMax', item.propertyPricePSMMax));
-            this.props.dispatch(change('filter', 'propertyPostCode', item.propertyPostCode));
-            this.props.dispatch(change('filter', 'propertyPriceToLandValueMin', item.propertyPriceToLandValueMin));
-            this.props.dispatch(change('filter', 'propertyPriceToLandValueMax', item.propertyPriceToLandValueMax));
-            this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMin', item.propertyFloorSpaceRatioMin));
-            this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMax', item.propertyPriceToLandValueMin));
-        }        
+// displayFilterParameters = (item) => {
+//             console.log(this.props)
+//             console.log(item)
+//             this.props.dispatch(change('filter', 'propertyZone', item.propertyZone));
+//             this.props.dispatch(change('filter', 'propertyAreaMin', item.propertyAreaMin));
+//             this.props.dispatch(change('filter', 'propertyAreaMax', item.propertyAreaMax));
+//             this.props.dispatch(change('filter', 'propertyPriceMin', item.propertyPriceMin));
+//             this.props.dispatch(change('filter', 'propertyPriceMax', item.propertyPriceMax));
+//             this.props.dispatch(change('filter', 'propertyPricePSMMin', item.propertyPricePSMMin));
+//             this.props.dispatch(change('filter', 'propertyPricePSMMax', item.propertyPricePSMMax));
+//             this.props.dispatch(change('filter', 'propertyPostCode', item.propertyPostCode));
+//             this.props.dispatch(change('filter', 'propertyPriceToLandValueMin', item.propertyPriceToLandValueMin));
+//             this.props.dispatch(change('filter', 'propertyPriceToLandValueMax', item.propertyPriceToLandValueMax));
+//             this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMin', item.propertyFloorSpaceRatioMin));
+//             this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMax', item.propertyPriceToLandValueMin));
+//         }        
 
     componentDidMount() {
         this.checkAuthentication();
