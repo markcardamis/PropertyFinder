7af674fc0d6e94093667899027c3f062639e7041
Iryna Paliy <irapaliy@gmail.com>
2019-12-17 01:18:02 +0200

    changes to property information: param and icons

--- src/main/webapp/components/widgets/PropertyInformation.js
+++ src/main/webapp/components/widgets/PropertyInformation.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { FaBed, FaBath, FaCar, FaChartArea, FaBuilding, FaLink } from 'react-icons/fa';
+import { FaBed, FaBath, FaCar, FaChartArea, FaBuilding, FaDoorOpen, FaInfo, FaFileInvoiceDollar, FaBalanceScale } from 'react-icons/fa';
 import { GiReceiveMoney, GiAustralia } from 'react-icons/gi';
-import { IoMdPricetags, IoMdClose } from 'react-icons/io';
+import { IoMdPricetags, IoMdClose, IoMdResize } from 'react-icons/io';
 
 import { DEFAULT_HOUSE_IMAGE } from '../../shared/constants';
 
@@ -19,61 +19,24 @@ handleClose = () => {
 
                 <div>
                     <ul className='propertyInformation col-lg-4'>
-                        <li className='justify-content-between' style={{display: 'flex'}}>
-                            <h4>Property ID: {property.id}</h4>
+                        <li><h4>Property ID: {property.id}</h4>
                             <IoMdClose size='2em' onClick={this.handleClose}/>
                         </li>
-                        <li>
-                            <img 
-                                src={property.image || DEFAULT_HOUSE_IMAGE} 
-                                style={{width: '150px', height: '150px'}}
-                            />
-                        </li>
-                        <li>
-                            <IoMdPricetags size='1.5em'/>
-                            <b> Price: </b>
-                            {property.price}
-                        </li>
-                        <li>
-                            <FaBed size='1.5em'/>
-                            <b> Bedrooms: </b>
-                            {property.bedroom}
-                        </li>
-                        <li>
-                            <FaBath size='1.5em'/>
-                            <b> Bathrooms: </b>
-                            {property.bathroom}
-                        </li>
-                        <li>
-                            <FaCar size='1.5em'/>
-                            <b> Car spaces: </b>
-                            {property.carspace}
-                        </li>
-                        <li>
-                            <FaChartArea size='1.5em'/>
-                            <b> Area: </b>
-                            {property.area}
-                        </li>
-                        <li>
-                            <GiAustralia size='1.5em'/>
-                            <b> Zone: </b>
-                            {property.zone}
-                        </li>
-                        <li>
-                            <FaBuilding size='1.5em'/>
-                            <b> FSR: </b>
-                            {property.fsr}
-                        </li>
-                        <li>
-                            <FaLink size='1.5em'/>
-                            <b> Domain URL: </b>
-                            <a href='#'>{property.domainurl}</a>
-                        </li>
-                        <li>
-                            <GiReceiveMoney size='1.5em'/>
-                            <b> Land Value: </b>
-                            {property.landvalue}
-                        </li>
+                        <li><img src={property.listingPhoto || DEFAULT_HOUSE_IMAGE} style={{width: '150px', height: '150px'}}/></li>
+                        {property.price && <li><IoMdPricetags size='1.5em'/><b> Price: </b>{property.price}</li>}
+                        {property.listingURL && <li><FaLink size='1.5em'/><b> URL: </b><a href={property.listingURL}>{property.listingURL}</a></li>}
+                        {property.address && <li><FaDoorOpen size='1.5em'/><b> Address: </b>{property.address}</li>}
+                        {property.area && <li><FaChartArea size='1.5em'/><b> Area: </b>{property.area}</li>}
+                        {property.bathrooms && <li><FaBath size='1.5em'/><b> Bathrooms: </b>{property.bathrooms}</li>}
+                        {property.bedrooms && <li><FaBed size='1.5em'/><b> Bedrooms: </b>{property.bedrooms}</li>}
+                        {property.carspaces && <li><FaCar size='1.5em'/><b> Car spaces: </b>{property.carspaces}</li>}
+                        {property.zone && <li><GiAustralia size='1.5em'/><b> Zone: </b>{property.zone}</li>}
+                        {property.floorSpaceRatio && <li><FaBuilding size='1.5em'/><b> Floor Space Ratio: </b>{property.floorSpaceRatio}</li>}
+                        {property.minimumLotSize && <li><IoMdResize size='1.5em'/><b> Minimum Lot Size: </b>{property.minimumLotSize}</li>}
+                        {property.landValue && <li><GiReceiveMoney size='1.5em'/><b> Land Value: </b>{property.landValue}</li>}
+                        {property.pricePSM && <li><FaFileInvoiceDollar size='1.5em'/><b> Price per m<sup>2</sup>: </b>{property.pricePSM}</li>}
+                        {property.priceToLandValue && <li><FaBalanceScale size='1.5em'/><b> Price To Land Value: </b>{property.priceToLandValue}</li>}
+                        {property.summaryDescription && <li><FaInfo size='1.5em'/><b> Description: </b>{property.summaryDescription}</li>}      
                     </ul> 
                 </div>
             )
