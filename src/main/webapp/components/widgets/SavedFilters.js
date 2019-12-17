b0c5c306160f41e1db30adee753fd97d4c15e0a4
markcardamis <markncardamis@gmail.com>
2019-12-17 17:47:01 +1100

    adjust indent and added method display pins when saved filter selected

--- src/main/webapp/components/widgets/SavedFilters.js
+++ src/main/webapp/components/widgets/SavedFilters.js
@@ -7,28 +7,27 @@ import { connect } from 'react-redux';
 // import { listSavedFilters } from '../../shared/api/api'
 
 class SavedFilters extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        authenticted: null,
+        savedFilters: []
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            authenticted: null,
-            savedFilters: []
-        };
-      }
-
-   checkAuthentication =  async () => {
-        const authenticated = await this.props.auth.isAuthenticated();
-        if (authenticated !== this.state.authenticated) {
-          this.setState({ authenticated });
-        }
+  checkAuthentication =  async () => {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
     }
+  }
       
   listSavedFilters = async () => {
     try {
-        const response = await fetch('/api/notifications', {
-          headers: {
-              Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
-          }
+      const response = await fetch('/api/notifications', {
+        headers: {
+          Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
+        }
       });
       const data = await response.json();
       console.dir({ data });
@@ -37,86 +36,91 @@ class SavedFilters extends Component {
       //   this.setState({ notifications : JSON.stringify(data) });
       this.setState({ savedFilters : data });
     } catch (err) {
-        console.log('error loading list of filters');
+      console.log('error loading list of filters');
     }
   }
 
   handleSelectFilter = async (item) => {
-      console.log('select filter works')
-      try {
-        const response = await fetch(`/api/listing/notifications/${item.id}`, {
-            headers: {
-              Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
-            }
-            });
-        const data = await response.json();
-        console.log(data)
-        this.displayFilterParameters(data);
-      } catch (err) {
-          console.log('error loading list of filters');
-        };
-    }
+    console.log('select filter works')
+    try {
+      const response = await fetch(`/api/listing/notifications/${item.id}`, {
+        headers: {
+          Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
+        }
+      });
+      const data = await response.json();
+      
+      this.props.dispatch({
+        type: 'MARKERS',
+        payload: data
+      });
 
-    displayFilterParameters = (item) => {
-      this.props.dispatch(change('filter', 'propertyZone', item.propertyZone));
-      this.props.dispatch(change('filter', 'propertyAreaMin', item.propertyAreaMin));
-      this.props.dispatch(change('filter', 'propertyAreaMax', item.propertyAreaMax));
-      this.props.dispatch(change('filter', 'propertyPriceMin', item.propertyPriceMin));
-      this.props.dispatch(change('filter', 'propertyPriceMax', item.propertyPriceMax));
-      this.props.dispatch(change('filter', 'propertyPricePSMMin', item.propertyPricePSMMin));
-      this.props.dispatch(change('filter', 'propertyPricePSMMax', item.propertyPricePSMMax));
-      this.props.dispatch(change('filter', 'propertyPostCode', item.propertyPostCode));
-      this.props.dispatch(change('filter', 'propertyPriceToLandValueMin', item.propertyPriceToLandValueMin));
-      this.props.dispatch(change('filter', 'propertyPriceToLandValueMax', item.propertyPriceToLandValueMax));
-      this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMin', item.propertyFloorSpaceRatioMin));
-      this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMax', item.propertyPriceToLandValueMin));
+      this.displayFilterParameters(data);
+    } catch (err) {
+      console.log('error loading list of filters');
+    }
+  }
+
+  displayFilterParameters = (item) => {
+    this.props.dispatch(change('filter', 'propertyZone', item.propertyZone));
+    this.props.dispatch(change('filter', 'propertyAreaMin', item.propertyAreaMin));
+    this.props.dispatch(change('filter', 'propertyAreaMax', item.propertyAreaMax));
+    this.props.dispatch(change('filter', 'propertyPriceMin', item.propertyPriceMin));
+    this.props.dispatch(change('filter', 'propertyPriceMax', item.propertyPriceMax));
+    this.props.dispatch(change('filter', 'propertyPricePSMMin', item.propertyPricePSMMin));
+    this.props.dispatch(change('filter', 'propertyPricePSMMax', item.propertyPricePSMMax));
+    this.props.dispatch(change('filter', 'propertyPostCode', item.propertyPostCode));
+    this.props.dispatch(change('filter', 'propertyPriceToLandValueMin', item.propertyPriceToLandValueMin));
+    this.props.dispatch(change('filter', 'propertyPriceToLandValueMax', item.propertyPriceToLandValueMax));
+    this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMin', item.propertyFloorSpaceRatioMin));
+    this.props.dispatch(change('filter', 'propertyFloorSpaceRatioMax', item.propertyPriceToLandValueMin));
   };
 
   handleDeleteFilter = async (item) => {
-      try {
-          const response = await fetch(`/api/notifications/${item.id}`, {
-            method: 'DELETE',
-            headers: {
-              Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
-            }
-          });
-          const data = await response.json();
-          console.dir({ data });
-    
-          //   this.setState({ notifications : JSON.stringify(data) });
-          // this.setState({ savedFilters : data });
-      } catch (err) {
-          console.log('error delete filter');
-      }
-      this.listSavedFilters();
+    try {
+      const response = await fetch(`/api/notifications/${item.id}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
+        }
+      });
+      const data = await response.json();
+      console.dir({ data });
+
+      // this.setState({ notifications : JSON.stringify(data) });
+      // this.setState({ savedFilters : data });
+    } catch (err) {
+      console.log('error delete filter');
     }
+    this.listSavedFilters();
+  }
 
   renderData = () => {
-      return this.state.savedFilters.map((item)=>
-            <li key={item.id} className='filterItem d-flex justify-content-between'>
-                <div>
-                  <div style={{display: 'flex'}}>
-                    <h5 onClick={()=>this.handleSelectFilter(item)}>Filter {this.state.savedFilters.indexOf(item)+1}</h5>
-                    <TiPencil className='filterItemIcon' size='1.3em' onClick={()=>this.props.onClick(item)}/>
-                    <TiTrash className='filterItemIcon' size='1.3em' onClick={()=>this.handleDeleteFilter(item)}/>
-                  </div>
-                    <ul onClick={this.handleSelectFilter.bind(this, item)} style={{fontSize: '12px', listStyle: 'none'}}>
-                        <li>{item.propertyZone ? `Zone: ${item.propertyZone}` : null}</li>
-                        <li>{item.propertyAreaMin ? `Area min: ${item.propertyAreaMin}` : null}</li>
-                        <li>{item.propertyAreaMax ? `Area max: ${item.propertyAreaMax}` : null}</li>
-                        <li>{item.propertyPriceMin ? `Price min: ${item.propertyPriceMin}` : null}</li>
-                        <li>{item.propertyPriceMax ? `Price max: ${item.propertyPriceMax}` : null}</li>
-                        <li>{item.propertyPSMMin ? `Price per m2 min: ${item.propertyPSMMin}` : null}</li>
-                        <li>{item.propertyPSMMax ? `Price per m2 max: ${item.propertyPSMMax}` : null}</li>  
-                        <li>{item.propertyPostCode ? `Post code: ${item.propertyPostCode}` : null}</li>
-                        <li>{item.propertyPriceToLandValueMin ? `Price to landvalue min: ${item.propertyPriceToLandValueMin}` : null}</li>
-                        <li>{item.propertyPriceToLandValueMax ? `Price to landvalue max: ${item.propertyPriceToLandValueMax}` : null}</li> 
-                        <li>{item.propertyFloorSpaceRatioMin ? `Floorspace ratio min: ${item.propertyFloorSpaceRatioMin}` : null}</li> 
-                        <li>{item.propertyFloorSpaceRatioMax ? `Floorspace ratio max: ${item.propertyFloorSpaceRatioMax}` : null}</li> 
-                    </ul>
-                </div>
-            </li>
-      );
+    return this.state.savedFilters.map((item)=>
+      <li key={item.id} className='filterItem d-flex justify-content-between'>
+        <div>
+          <div style={{display: 'flex'}}>
+            <h5 onClick={()=>this.handleSelectFilter(item)}>Filter {this.state.savedFilters.indexOf(item)+1}</h5>
+            <TiPencil className='filterItemIcon' size='1.3em' onClick={()=>this.props.onClick(item)}/>
+            <TiTrash className='filterItemIcon' size='1.3em' onClick={()=>this.handleDeleteFilter(item)}/>
+          </div>
+          <ul onClick={this.handleSelectFilter.bind(this, item)} style={{fontSize: '12px', listStyle: 'none'}}>
+            <li>{item.propertyZone ? `Zone: ${item.propertyZone}` : null}</li>
+            <li>{item.propertyAreaMin ? `Area min: ${item.propertyAreaMin}` : null}</li>
+            <li>{item.propertyAreaMax ? `Area max: ${item.propertyAreaMax}` : null}</li>
+            <li>{item.propertyPriceMin ? `Price min: ${item.propertyPriceMin}` : null}</li>
+            <li>{item.propertyPriceMax ? `Price max: ${item.propertyPriceMax}` : null}</li>
+            <li>{item.propertyPSMMin ? `Price per m2 min: ${item.propertyPSMMin}` : null}</li>
+            <li>{item.propertyPSMMax ? `Price per m2 max: ${item.propertyPSMMax}` : null}</li>  
+            <li>{item.propertyPostCode ? `Post code: ${item.propertyPostCode}` : null}</li>
+            <li>{item.propertyPriceToLandValueMin ? `Price to landvalue min: ${item.propertyPriceToLandValueMin}` : null}</li>
+            <li>{item.propertyPriceToLandValueMax ? `Price to landvalue max: ${item.propertyPriceToLandValueMax}` : null}</li> 
+            <li>{item.propertyFloorSpaceRatioMin ? `Floorspace ratio min: ${item.propertyFloorSpaceRatioMin}` : null}</li> 
+            <li>{item.propertyFloorSpaceRatioMax ? `Floorspace ratio max: ${item.propertyFloorSpaceRatioMax}` : null}</li> 
+          </ul>
+        </div>
+      </li>
+    );
   }
 
   componentDidMount() {
@@ -124,18 +128,19 @@ class SavedFilters extends Component {
     this.listSavedFilters();
   }
 
-    render() {
-      return (
-        <div>
-          <ul className='savedFiltersList col-lg-12'>{this.renderData()}</ul>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <ul className='savedFiltersList col-lg-12'>{this.renderData()}</ul>
+      </div>
+    )
+  }
+
 }
 
 const mapStateToProps = (state) => {
   return {
-      savedFilters: state
+    savedFilters: state
   };
 };
 
