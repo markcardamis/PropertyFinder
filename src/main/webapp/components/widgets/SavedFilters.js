59a26e19e08b3b3ba73a85fd2babe2f45471e235
Iryna Paliy <irapaliy@gmail.com>
2019-12-17 03:51:07 +0200

    fixes

--- src/main/webapp/components/widgets/SavedFilters.js
+++ src/main/webapp/components/widgets/SavedFilters.js
@@ -23,23 +23,23 @@ class SavedFilters extends Component {
         }
     }
       
-  // async listSavedFilters () {
-  //   try {
-  //       const response = await fetch('/api/notifications', {
-  //         headers: {
-  //             Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
-  //         }
-  //     });
-  //     const data = await response.json();
-  //     console.dir({ data });
-  //     console.log('successfully loaded list of filters');
+  async listSavedFilters () {
+    try {
+        const response = await fetch('/api/notifications', {
+          headers: {
+              Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
+          }
+      });
+      const data = await response.json();
+      console.dir({ data });
+      console.log('successfully loaded list of filters');
 
-  //     //   this.setState({ notifications : JSON.stringify(data) });
-  //     this.setState({ savedFilters : data });
-  //   } catch (err) {
-  //       console.log('error loading list of filters');
-  //   }
-  // }
+      //   this.setState({ notifications : JSON.stringify(data) });
+      this.setState({ savedFilters : data });
+    } catch (err) {
+        console.log('error loading list of filters');
+    }
+  }
 
   async handleSelectFilter (item) {
       console.log('select filter works')
