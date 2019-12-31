2a2e613e843eced733759d026a7df2ea9ca925f9
markcardamis <markncardamis@gmail.com>
2019-12-31 18:17:48 +1100

    reverse hunk

--- src/main/java/com/majoapps/propertyfinder/data/repository/PropertyListingRepository.java
+++ src/main/java/com/majoapps/propertyfinder/data/repository/PropertyListingRepository.java
@@ -3,12 +3,12 @@ package com.majoapps.propertyfinder.data.repository;
 import com.majoapps.propertyfinder.data.entity.PropertyListing;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PropertyListingRepository extends JpaRepository<PropertyListing, Integer> {
-    List<PropertyListing> findTop100ByOrderByIdAsc();
-    List<PropertyListing> findTop1000ByOrderByIdAsc();
+public interface PropertyListingRepository extends JpaRepository<PropertyListing, Integer>, 
+        JpaSpecificationExecutor<PropertyListing> {
     List<PropertyListing> findByDomainListingId(Integer domainListingId);
     List<PropertyListing> findByPlanningPortalPropId(String planningPortalPropId);
 }
