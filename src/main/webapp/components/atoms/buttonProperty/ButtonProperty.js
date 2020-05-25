fa5d03baf1bbad4e35d2abe94051f4f04957a1a2
markcardamis <markncardamis@gmail.com>
2020-05-25 22:57:03 +1000

    renamed files back with correct case

--- src/main/webapp/components/atoms/buttonProperty/ButtonProperty.js
+++ src/main/webapp/components/atoms/buttonProperty/ButtonProperty.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './buttonProperty1.scss';
+import './buttonProperty.scss';
 
 const ButtonProperty = props => {
     return (
--- src/main/webapp/components/molecules/filterButtonGroup/FilterButtonGroup.js
+++ src/main/webapp/components/molecules/filterButtonGroup/FilterButtonGroup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ButtonSquare from '../../atoms/buttonSquare/ButtonSquare';
 import {IconFilter2, IconMenu} from '../../../assets/icons';
-import './filterButtonGroup1.scss';
+import './filterButtonGroup.scss';
 
 const FilterButtonGroup = props => {
     return (
