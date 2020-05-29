1589fb75172c22c1ff83005eeaaf1ae1d178697c
Iryna Paliy <irapaliy@gmail.com>
2020-05-30 02:57:05 +0300

    slider tooltip

--- src/main/webapp/components/organisms/map/MapGL.js
+++ src/main/webapp/components/organisms/map/MapGL.js
@@ -152,7 +152,6 @@ checkAuthentication = async () => {
     render() {
     return (
         <div>
-            {/* {console.log(this.props.mapGL.showLoading)} */}
             <div ref={el => this.mapContainer = el} className='mapContainer' id='map'/>
         </div>
         );
