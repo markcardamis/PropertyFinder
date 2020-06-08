fe9d58faffafbc458a8d4c2e9e3eabfe2cd23a79
markcardamis <markncardamis@gmail.com>
2020-06-09 02:04:53 +1000

    added fields back in the property listing DTO

--- src/main/java/com/majoapps/propertyfinder/business/domain/PropertyListingDTO.java
+++ src/main/java/com/majoapps/propertyfinder/business/domain/PropertyListingDTO.java
@@ -12,6 +12,22 @@ public class PropertyListingDTO implements Serializable {
 
     @JsonProperty("id") private Integer id;
     @JsonProperty("latitude") private Double latitude;
-    @JsonProperty("longitude") private Double longitude;    
+    @JsonProperty("longitude") private Double longitude;  
+    @JsonProperty("price") private String price;
+    @JsonProperty("listing_url") private String listingURL;
+    @JsonProperty("listing_photo") private String listingPhoto;
+    @JsonProperty("address") private String address;
+    @JsonProperty("area") private Integer area;
+    @JsonProperty("bathrooms") private Double bathrooms;
+    @JsonProperty("bedrooms") private Double bedrooms;
+    @JsonProperty("carspaces") private Integer carspaces;
+    @JsonProperty("summary_description") private String summaryDescription;
+    @JsonProperty("planning_portal_id") private String planningPortalPropId;
+    @JsonProperty("zone") private String zone;
+    @JsonProperty("floor_space_ratio") private BigDecimal floorSpaceRatio;
+    @JsonProperty("minimum_lot_size") private String minimumLotSize;
+    @JsonProperty("land_value") private Integer landValue;
+    @JsonProperty("price_psm") private Integer pricePSM;
+    @JsonProperty("price_to_land_value") private BigDecimal priceToLandValue;
 }
 
