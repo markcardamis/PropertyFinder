1c8cc4aed94e1bf67d22ccaf16ceeaf9c8a946ff
Iryna Paliy <irapaliy@gmail.com>
2020-07-07 09:37:07 +0300

    auth mobile

--- src/main/webapp/components/organisms/mobileNav/MobileNav.js
+++ src/main/webapp/components/organisms/mobileNav/MobileNav.js
@@ -1,15 +1,11 @@
 import React, {useState} from 'react'
 import {Link, useLocation} from 'react-router-dom';
-import PropTypes from 'prop-types'
-import  {useDispatch, useSelector} from 'react-redux'
 import { withAuth } from '@okta/okta-react';
 import './mobileNav.scss'
 import { useAuth } from '../../../modules/auth';
 import TopNavList from '../../molecules/topNavList/TopNavList'
 import ButtonLogin from '../../atoms/buttonLogin/ButtonLogin';
 import ButtonAccount from '../../atoms/buttonAccount/ButtonAccount';
-import { closeMobileNav } from '../../../store/actions/showMobileNavAction';
-import {showSignIn} from '../../../store/actions/signInAction'
 import LoginForm from '../../molecules/loginForm/LoginForm';
 import Account from '../../molecules/account/Account';
 import UserInfo from '../../molecules/userInfo/UserInfo';
@@ -21,10 +17,6 @@ const MobileNav = withAuth(({ auth }) => {
     const [state, setState] = useState('nav')
     const location = useLocation();
 
-    // const handleClick = () => {
-    //     dispatch(closeMobileNav())
-    //     dispatch(showSignIn())
-    // }
     const renderComponent = () => {
         if (state=='login') {
              return authenticated ?  <div className='mobileNav_formContainer'><Account onLogout={()=>{auth.logout(); setState('login')}} onAccountClick={()=>setState('account')}/></div> :
