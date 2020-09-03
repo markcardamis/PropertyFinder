0dacf2684d2957919ccaac693a8529df18bafc4f
markcardamis <markncardamis@gmail.com>
2020-09-03 19:44:08 +1000

    added land value descriptions

--- src/main/webapp/shared/constants.js
+++ src/main/webapp/shared/constants.js
@@ -24,80 +24,82 @@ export const BEDROOMS = 'Bedrooms';
 export const CAR_SPACES = 'Car Spaces';
 
 
-export const ZONES = [{id: 1, name: 'B1', color: '#f4fefd'}, {
-                        id: 2, name: 'B2', color: '#e1fafc'
+export const ZONES = [
+                    {
+                        id: 1, name: 'B1 - Neighbourhood Centre', color: '#f4fefd'
                     }, {
-                        id: 3, name: 'B3', color: '#d4eff9'
+                        id: 2, name: 'B2 - Local Centre', color: '#e1fafc'
                     }, {
-                        id: 4, name: 'B4', color: '#e5e7ef'
+                        id: 3, name: 'B3 - Commercial Core', color: '#d4eff9'
                     }, {
-                        id: 5, name: 'B5', color: '#e0e7e9'
+                        id: 4, name: 'B4 - Mixed Use', color: '#e5e7ef'
                     }, {
-                        id: 6, name: 'B6', color: '#e6eef1'
+                        id: 5, name: 'B5 - Business Development', color: '#e0e7e9'
                     }, {
-                        id: 7, name: 'B7', color: '#eff4f6'
+                        id: 6, name: 'B6 - Enterprise Corridor', color: '#e6eef1'
                     }, {
-                        id: 8, name: 'B8', color: '#f2f8f7'
+                        id: 7, name: 'B7 - Business Park', color: '#eff4f6'
                     }, {
-                        id: 9, name: 'DM', color: '#ffffff'
+                        id: 8, name: 'B8 - Metropolitan Centre', color: '#f2f8f7'
                     }, {
-                        id: 10, name: 'E1', color: '#f6e6cd'
+                        id: 9, name: 'DM - Deferred Matter', color: '#ffffff'
                     }, {
-                        id: 11, name: 'E2', color: '#f9ebd4'
+                        id: 10, name: 'E1 - National Parks and Nature Reserves', color: '#f6e6cd'
                     }, {
-                        id: 12, name: 'E3', color: '#fbf0dc'
+                        id: 11, name: 'E2 - Environmental Conservation', color: '#f9ebd4'
                     }, {
-                        id: 13, name: 'E4', color: '#fdf6e7'
+                        id: 12, name: 'E3 - Environmental Management', color: '#fbf0dc'
                     }, {
-                        id: 14, name: 'IN1', color: '#f5eefb'
+                        id: 13, name: 'E4 - Environmental Living', color: '#fdf6e7'
                     }, {
-                        id: 15, name: 'IN2', color: '#fbf6fe'
+                        id: 14, name: 'IN1 - General Industrial', color: '#f5eefb'
                     }, {
-                        id: 16, name: 'IN3', color: '#eee5f8'
+                        id: 15, name: 'IN2 - Light Industrial', color: '#fbf6fe'
                     }, {
-                        id: 17, name: 'IN4', color: '#e8ddf5'
+                        id: 16, name: 'IN3 - Heavy Industrial', color: '#eee5f8'
                     }, {
-                        id: 18, name: 'R1', color: '#fcf3fe'
+                        id: 17, name: 'IN4 - Working Waterfront', color: '#e8ddf5'
                     }, {
-                        id: 19, name: 'R2', color: '#fbeae8'
+                        id: 18, name: 'R1 - General Residential', color: '#fcf3fe'
                     }, {
-                        id: 20, name: 'R3', color: '#fadfdb'
+                        id: 19, name: 'R2 - Low Density Residential', color: '#fbeae8'
                     }, {
-                        id: 17, name: 'R4', color: '#fad5d0'
+                        id: 20, name: 'R3 - Medium Density Residential', color: '#fadfdb'
                     }, {
-                        id: 17, name: 'R5', color: '#e8ddf5'
+                        id: 17, name: 'R4 - High Density Residential', color: '#fad5d0'
                     }, {
-                        id: 18, name: 'RE1', color: '#e1fdd2'
+                        id: 17, name: 'R5 - Large Lot Residential', color: '#e8ddf5'
                     }, {
-                        id: 19, name: 'RE2', color: '#f6fef0'
+                        id: 18, name: 'RE1 - Public Recreation', color: '#e1fdd2'
                     }, {
-                        id: 20, name: 'RU1', color: '#faf5ec'
+                        id: 19, name: 'RE2 - Private Recreation', color: '#f6fef0'
                     }, {
-                        id: 21, name: 'RU2', color: '#f7f2e6'
+                        id: 20, name: 'RU1 - Primary Production', color: '#faf5ec'
                     }, {
-                        id: 22, name: 'RU3', color: '#f5efe1'
+                        id: 21, name: 'RU2 - Rural Landscape', color: '#f7f2e6'
                     }, {
-                        id: 23, name: 'RU4', color: '#f4eedd'
+                        id: 22, name: 'RU3 - Forestry', color: '#f5efe1'
                     }, {
-                        id: 24, name: 'RU5', color: '#f2e8e7'
+                        id: 23, name: 'RU4 - Rural Small Holdings', color: '#f4eedd'
                     }, {
-                        id: 25, name: 'RU6', color: '#efe7d5'
+                        id: 24, name: 'RU5 - Village', color: '#f2e8e7'
                     }, {
-                        id: 26, name: 'SP1', color: '#ffffea'
+                        id: 25, name: 'RU6 - Transition', color: '#efe7d5'
                     }, {
-                        id: 27, name: 'SP2', color: '#fffee0'
+                        id: 26, name: 'SP1 - Special Activities', color: '#ffffea'
                     }, {
-                        id: 28, name: 'SP3', color: '#fffed4'
+                        id: 27, name: 'SP2 - Infrastructure', color: '#fffee0'
                     }, {
-                        id: 29, name: 'UD', color: '#fbe0da'
+                        id: 28, name: 'SP3 - Tourist', color: '#fffed4'
                     }, {
-                        id: 30, name: 'UL', color: '#ffffff'
+                        id: 29, name: 'UL - Unzoned Land', color: '#ffffff'
                     }, {
-                        id: 31, name: 'W1', color: '#ebfef7'
+                        id: 30, name: 'W1 - Natural Waterways', color: '#ebfef7'
                     }, {
-                        id: 32, name: 'W2', color: '#ebfef7'
+                        id: 31, name: 'W2 - Recreational Waterways', color: '#ebfef7'
                     }, {
-                        id: 33, name: 'W3', color: '#defdef'
+                        id: 32, name: 'W3 - Working Waterways', color: '#defdef'
                     }, {
-                        id: 34, name: 'Non Standard Zones', color: 'lightgray'
-                    }]
+                        id: 33, name: 'Non Standard Zones', color: 'lightgray'
+                    }
+                ];
