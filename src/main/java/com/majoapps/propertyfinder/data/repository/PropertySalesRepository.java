d9d1f989bbca8f2f48198ae6df08c65f3ff94bc2
markcardamis <markncardamis@gmail.com>
2020-09-27 00:43:41 +1000

    update chart sales to include only last 12 years data

--- src/main/java/com/majoapps/propertyfinder/data/repository/PropertySalesRepository.java
+++ src/main/java/com/majoapps/propertyfinder/data/repository/PropertySalesRepository.java
@@ -3,10 +3,12 @@ package com.majoapps.propertyfinder.data.repository;
 import com.majoapps.propertyfinder.data.entity.PropertySales;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PropertySalesRepository extends JpaRepository<PropertySales, Integer> {
     List<PropertySales> findByPropertyId(Integer propertyId);
-    List<PropertySales> findByPropertyIdOrderBySettlementDateDesc(Integer propertyId);
+    List<PropertySales> findByPropertyIdAndSettlementDateGreaterThanOrderBySettlementDateDesc(
+            Integer propertyId, Date settlementDate);
 }
