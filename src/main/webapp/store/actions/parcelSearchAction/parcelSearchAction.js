8d63b839a835d7fdee9e6c203bb40c98931d016a
Iryna Paliy <irapaliy@gmail.com>
2020-11-27 11:58:11 +0200

    added handleDisplayParcels function on searh click

--- src/main/webapp/store/actions/parcelSearchAction/parcelSearchAction.js
+++ src/main/webapp/store/actions/parcelSearchAction/parcelSearchAction.js
@@ -1,10 +1,22 @@
 import { store } from "../../../../webapp/javascript/index";
+import { map } from "../../../components/organisms/map/MapGL";
 import { hideLoading, showLoading } from "../loadingAction";
+
 const apiUrl = "/api/propertyinformation";
 
 export const applyParcelSearch = () => async dispatch => {
     const {zone, postCode, area, buildingHeight, landValue, floorspaceRatio} = store.getState().parcelSearch;
     const {latitude, longitude} = store.getState().viewport;
+    const parcels = store.getState().parcelSearch.result;
+
+    const handleDisplayParcels = () => {
+      let array = parcels.map(item => item.property_id);
+      
+      if (array.length > 0) {
+          let filter = [ 'in', [ 'get', 'propid' ], [ 'literal', array ] ];
+          map.setFilter('nsw-property-highlighted', filter);
+      }
+    }
 
     const values = {
         zone: `zoneCode=${zone !== null ? zone : ''}`,
@@ -27,6 +39,7 @@ export const applyParcelSearch = () => async dispatch => {
         .then(response => response.json())
         .then(res=>dispatch({type: "PARCEL_SEARCH_LOADED", payload: res}))
         .catch(error => console.log(error));
+    handleDisplayParcels();
     dispatch(hideLoading());
   };
   
